1d104c698e08652184db6d5309982e5a1dd3f357
Justin Stanley <jtst@iastate.edu>
2023-04-24 22:32:18 -0500

    add dqn reference

--- dqn.py
+++ dqn.py
@@ -3,6 +3,9 @@ from pfrl import utils, explorers
 from pfrl.agents import DQN
 from torch import optim
 
+# DQN agent, example at
+# https://github.com/pfnet/pfrl/blob/2ad3d51a7a971f3fe7f2711f024be11642990d61/examples/gym/train_dqn_gym.py#L175
+
 def dqn_agent(env, agent):
     obs_space = env.observation_space(agent)
     action_space = env.action_space(agent)
