785846d5256db64df5745e8fa0bf2eee8b4ce7c6
juliachen2357 <95378237+juliachen2357@users.noreply.github.com>
2023-04-26 17:18:42 -0700

    Add files via upload

--- PPO.py
+++ PPO.py
@@ -1,78 +1,85 @@
-from pfrl import q_functions, replay_buffers
-from pfrl import utils, explorers
-from pfrl.agents import DQN
-from torch import optim
-
-# DQN agent, example at
-# https://github.com/pfnet/pfrl/blob/2ad3d51a7a971f3fe7f2711f024be11642990d61/examples/gym/train_dqn_gym.py#L175
-
-import argparse
-import functools
-
-import gym
-import gym.spaces
 import numpy as np
+
+from torch import optim
+import torch.nn as nn
+import torch.nn.functional as F
 import torch
 from torch import nn
-
+from pfrl import nn as pnn
+from pfrl import agents, q_functions, replay_buffers, explorers, action_value
+from pfrl.policies import SoftmaxCategoricalHead
 import pfrl
-from pfrl import experiments, utils
-from pfrl.agents import PPO
 
-def ppo_agent(env, agent):
-    obs_space = env.observation_space(agent)
-    action_space = env.action_space(agent)
 
-    print("Observation space:", obs_space)
-    print("Action space:", action_space)
+def PPO_agent(env, agent, steps):
 
-    # initialize Q function
-    policy = torch.nn.Sequential(
-            nn.Linear(obs_space.shape[0], 64),
-            nn.Tanh(),
-            nn.Linear(64, 64),
-            nn.Tanh(),
-            nn.Linear(64, action_space.n),
-            pfrl.policies.GaussianHeadWithStateIndependentCovariance(
-                action_size=action_space,
-                var_type="diagonal",
-                var_func=lambda x: torch.exp(2 * x),  # Parameterize log std
-                var_param_init=0,  # log std = 0 => std = 1
-            ),
+    obs_size = env.observation_space(agent).shape[0]
+    n_actions = env.action_space(agent).n
+
+    model = nn.Sequential(
+        nn.Linear(obs_size, 50),
+        nn.ReLU(),
+        nn.Linear(50, 50),
+        nn.ReLU(),
+        nn.Linear(50, 50),
+        nn.ReLU(),
+        nn.Linear(50, 50),
+        nn.ReLU(),
+        pfrl.nn.Branched(
+            nn.Sequential(
+                nn.Linear(50, n_actions,
+                SoftmaxCategoricalHead(),
+            ),)),
+            nn.Linear(50, 1),
         )
+ 
 
-    vf = torch.nn.Sequential(
-        nn.Linear(obs_space.shape[0], 64),
-        nn.Tanh(),
-        nn.Linear(64, 64),
-        nn.Tanh(),
-        nn.Linear(64, 1),
-    )
 
-    # epsilon-greedy exploration
-    explorer = explorers.ConstantEpsilonGreedy(
-        epsilon=0.3, random_action_func=action_space.sample
-    )
+    # vf = torch.nn.Sequential(
+    #     nn.Linear(obs_size, 64),
+    #     nn.Tanh(),
+    #     nn.Linear(64, 64),
+    #     nn.Tanh(),
+    #     nn.Linear(64, 1),
+    # )
+    # model = pfrl.nn.Branched( model, vf)
+    # Noisy nets; Sigma is the scaling factor of the initial weights of noise-scaling parameters
 
-    # DQN agent
-    model = pfrl.nn.Branched(policy, vf)
 
+    # Optimizer; Use the same hyper parameters as https://arxiv.org/abs/1710.02298
     opt = torch.optim.Adam(model.parameters(), lr=3e-4, eps=1e-5)
+
+    # Prioritized Replay
+    # Anneal beta from beta0 to 1 throughout training
+    update_interval = 4
+    betasteps = steps / update_interval
+
+    # Capacity in terms of number of transitions, Exponent of errors to compute probabilities to sample
+    # Initial value of beta, Steps to anneal beta to 1 
+    # Divide by the maximum weight in the memory to normalize weights
+    rbuf = replay_buffers.PrioritizedReplayBuffer(10**6)
+
+    # Feature extractor
+    def phi(x):
+        return np.asarray(x, dtype=np.float32) / 255
+
+    Agent = agents.PPO
     obs_normalizer = pfrl.nn.EmpiricalNormalization(
-        obs_space.low.size, clip_threshold=5
+        obs_size, clip_threshold=5
     )
-    agent = PPO(
+    # target_update_interval: Determines how often the target Q-network is updated with the weights of the online Q-network
+    # update_interval: This parameter determines how often the online Q-network is updated with the experience collected in the replay buffer
+    #agent = Agent(q_func, opt, rbuf, gpu=-1, gamma=0.99, explorer=explorer, target_update_interval=100, update_interval=1, phi=phi)
+    agent = Agent(
         model,
         opt,
         obs_normalizer=obs_normalizer,
         gpu=-1,
-        update_interval=1,
-        minibatch_size=128,
-        epochs=10,
+        update_interval=100,
         clip_eps_vf=None,
         entropy_coef=0,
         standardize_advantages=True,
-        gamma=0.99,
+        gamma=0.995,
         lambd=0.97,
     )
 
